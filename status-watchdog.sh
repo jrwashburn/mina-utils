3cc8424a4ee4eccd892455a7299a00f6fe696264
[Ar][Te]m.[Be][Rn][At]skyy <artem.bernatskyy@gmail.com>
2021-01-06 01:21:50 +0200

    minor style changes and type fixes

--- status-watchdog.sh
+++ status-watchdog.sh
@@ -7,52 +7,51 @@ TOTALOFFLINECOUNT=0
 
 function Update_Peers() {
   wget -O ~/new-peers.txt "${PEERSURL}"
-  if ! diff -q peers.txt new-peers.txt ; then 
+  if ! diff -q peers.txt new-peers.txt; then
     echo "updating peers"
     cp new-peers.txt peers.txt
     cat peers.txt | xargs -I % coda advanced add-peers %
   else
     echo "peers are the same - not adding to daemon"
-  fi  
+  fi
 }
 
-while :
-do
+while :; do
   date
   STAT="$(coda client status -json | jq .sync_status)"
 
-  if [[ "$STAT" == "\"Synced\"" ]] ; then
+  if [[ "$STAT" == "\"Synced\"" ]]; then
     echo "In Sync-all good"
     OFFLINECOUNT=0
     CONNECTINGCOUNT=0
   fi
 
-  if [[ "$STAT" == "\"Connecting\"" ]] ; then
+  if [[ "$STAT" == "\"Connecting\"" ]]; then
     echo "Connecting"
     ((CONNECTINGCOUNT++))
     ((TOTALCONNECTINGCOUNT++))
   fi
 
-  if [[ "$STAT" == "\"Offline\"" ]] ; then
+  if [[ "$STAT" == "\"Offline\"" ]]; then
     echo "Offline"
     ((OFFLINECOUNT++))
     ((TOTALOFFLINECOUNT++))
     Update_Peers
   fi
-  
-  if [[ "$CONNECTINGCOUNT" > 1 ]] ; then
+
+  if [[ "$CONNECTINGCOUNT" > 1 ]]; then
     echo "Connecting - recycle"
     systemctl --user restart mina
     CONNECTINGCOUNT=0
   fi
 
-  if [[ "$OFFLINECOUNT" > 3 ]] ; then
+  if [[ "$OFFLINECOUNT" > 3 ]]; then
     echo "Offline - recycle - should never happen"
     systemctl --user restart mina
     OFFLINECOUNT=0
   fi
 
   echo "Status:" $STAT, "Connecting Count, Total:" $CONNECTINGCOUNT $TOTALCONNECTINGCOUNT, "Offline Count, Total:" $OFFLINECOUNT $TOTALOFFLINECOUNT
-  echo "sleepting for 5 mins"
+  echo "sleeping for 5 mins"
   sleep 300s
 done
