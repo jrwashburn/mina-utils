9594c3a1f11a420973ce9a94ff5c322b3c6e717d
Jonathan Washburn <jrwashburn@gmail.com>
2021-01-09 09:38:26 -0500

    adding node-upgrade script and disclaimer

--- node-upgrade.sh
+++ node-upgrade.sh
@@ -1,8 +1,10 @@
+BUILD="mina-testnet-postake-medium-curves=0.2.2-1-b14e324"
+
 systemctl --user stop mina
-echo Y | sudo apt-get remove mina-testnet-postake-medium-curves
+sudo apt-get remove -y mina-testnet-postake-medium-curves
 echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
 sudo apt-get update
-sudo apt-get install -y curl mina-testnet-postake-medium-curves=0.2.2+-hotfix-super-catchup-restart-libp2p-upload-blocks-9614ad9 --allow-downgrades
+sudo apt-get install -y curl unzip $BUILD --allow-downgrades
 systemctl --user daemon-reload
 systemctl --user start mina
 systemctl --user status mina
