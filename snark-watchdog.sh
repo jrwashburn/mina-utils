cc24a7b75e7de21fd423eea05ecce16499ce268e
Jonathan Washburn <jrwashburn@gmail.com>
2021-02-11 21:45:43 -0500

    env for coordinator

--- snark-watchdog.sh
+++ snark-watchdog.sh
@@ -5,10 +5,10 @@ do
     echo "More than 2 coda running, we have at least 1 snark workers"
   else
     echo "starting 4 snark workers"
-    coda internal snark-worker -daemon-address mina1.jkw.fm:8301 -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker1.log &
-    coda internal snark-worker -daemon-address mina1.jkw.fm:8301 -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker2.log &
-    coda internal snark-worker -daemon-address mina1.jkw.fm:8301 -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker3.log &
-    coda internal snark-worker -daemon-address mina1.jkw.fm:8301 -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker4.log &
+    coda internal snark-worker -daemon-address $MINA_COORDINATOR -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker1.log &
+    coda internal snark-worker -daemon-address $MINA_COORDINATOR -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker2.log &
+    coda internal snark-worker -daemon-address $MINA_COORDINATOR -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker3.log &
+    coda internal snark-worker -daemon-address $MINA_COORDINATOR -proof-level full -shutdown-on-disconnect false >>~/logs/snarkworker4.log &
     disown $( jobs -p )
   fi
   sleep 300s  
