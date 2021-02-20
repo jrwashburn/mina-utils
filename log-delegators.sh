ffdd62b39ea70d9d11a710d494ef053ffa44c28e
Jonathan Washburn <jrwashburn@gmail.com>
2021-02-19 21:43:58 -0500

    get list of delegators to a pool address with log-delegators.sh

--- log-delegators.sh
+++ log-delegators.sh
@@ -1,8 +1,7 @@
-#log delegators
 POOLADDRESS='B62YOURPOOL';
 TODAY=`date +%Y%m%d-%H%M%S`;
-STAKINGFILE=~/logs/staking-ledger-"$today.json";
-DELEGATORSFILE=~/logs/delegators-"$today.json";
+STAKINGFILE=~/logs/staking-ledger-"$TODAY.json";
+DELEGATORSFILE=~/logs/delegators-"$TODAY.json";
 
 coda ledger export staking-epoch-ledger > "$STAKINGFILE";
 
