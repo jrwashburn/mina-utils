6cae12821910835f45b855d4d7a3b7747fb00d66
Jonathan Washburn <jrwashburn@gmail.com>
2021-02-21 22:46:06 -0500

    added archive watcher - does not restart yet

--- status-watchdog.sh
+++ status-watchdog.sh
@@ -1,8 +1,10 @@
 STAT=""
+ARCHIVESTAT=0
 CONNECTINGCOUNT=0
 OFFLINECOUNT=0
 TOTALCONNECTINGCOUNT=0
 TOTALOFFLINECOUNT=0
+ARCHIVEDOWNCOUNT=0
 
 while :; do
   date
@@ -38,6 +40,16 @@ while :; do
     systemctl --user restart mina
     OFFLINECOUNT=0
   fi
+
+  ARCHIVERUNNING=`ps -A | grep coda-archive | wc -l`
+
+  if [[ "$ARCHIVERUNNING" > 0 ]]; then
+    echo "Archive is Running - ok"
+    ARCHIVERRUNNING=0
+  else
+    echo "ARCHIVE IS NOT RUNNING - ERROR"
+    ((ARCHIVEDOWNCOUNT++))
+  fi 
 
   echo "Status:" $STAT, "Connecting Count, Total:" $CONNECTINGCOUNT $TOTALCONNECTINGCOUNT, "Offline Count, Total:" $OFFLINECOUNT $TOTALOFFLINECOUNT
   echo "sleeping for 5 mins"
